ffc187f9dd17607b665c24fba97dcf36aba2bf0b
sebastian@weareher.com <sebastian@weareher.com>
2021-06-29 07:45:03 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sebastianMurdoch/whitelist-app
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/heroku/x v0.0.31
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "pong")
+	})
+
+	router.Run(":" + port)
+}
